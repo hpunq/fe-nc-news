8aa8e900d9716220ff457fa0b49b91b7dfb5bb91
hpunq <128979576+hpunq@users.noreply.github.com>
2024-12-17 11:21:21 +0000

    display a basic full article view

--- src/api.js
+++ src/api.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://nc-news-api-bh.onrender.com/api",
-})
+  baseURL: "https://nc-news-api-bh.onrender.com/api",
+});
 
 export function getArticles() {
-    return api.get("/articles").then(({ data }) => {
-      return data;
-    });
-  }
+  return api.get("/articles").then(({ data }) => {
+    return data;
+  });
+}
+
+export function getArticlePage(article_id) {
+  return api.get("/articles/" + (article_id)).then(({ data }) => {
+    return data.article;
+  }).catch((err) => {
+    console.log(err)
+  })
+}
